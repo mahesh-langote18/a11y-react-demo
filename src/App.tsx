31e11c14d8ff7618b0ec24e6e1364337302a9238
Mahesh Langote <mahesh.langote@arieotech.com>
2025-06-30 15:08:46 +0530

    router change

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
+import { HashRouter as Router, Route, Routes, Navigate } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import BuggyPage from "./components/BuggyPage";
 import FixedPage from "./components/FixedPage";
